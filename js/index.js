a897346eb59c1575180e42b701f519ca2485e5bb
i-c0112 <a7807118@gmail.com>
2015-05-27 11:06:02 +0800

    roughly implemented checkbox logic

--- js/index.js
+++ js/index.js
@@ -1,22 +1,31 @@
 var jq = jQuery.noConflict();
-jq(function() {
+jq(function(){
   'use strict';
-  var player = videojs('really-cool-video').ready(function() {
+  var player = videojs('really-cool-video').ready(function(){
     var p = this;
     var track = this.textTracks()[0];
     track.mode="hidden";
 
     // How about an event listener?
-    this.on('timeupdate', function() {
+    this.on('timeupdate', function(){
       try{
         let cues = track.activeCues;
-        if (cues && cues.length > 0) {
+        if (cues && cues.length > 0){
           let vtts = '';
-          for (let i=0; i < cues.length; ++i) {
+          for (let i=0; i < cues.length; ++i){
             let cue = cues[i];
             let vtt = cue.text;
             vtts += '<br>' + vtt;
-            if (vtt.substring(0,8)=="[George]"){
+
+            // In vtt: '[' + character_name + ']'
+            // checkbox id: 'mute-' + character_name
+            let end_index = vtt.indexOf(']');
+            if (end_index === -1)
+              throw "Failed to parse vtt file!";
+            let jq_el = jq('#mute-' + vtt.substring(1, end_index));
+            if (jq_el.length === 0)
+              throw "Character not found in checkbox!";
+            if (jq_el.attr("checked") == 'checked'){
               p.muted(true);
             }
           }
